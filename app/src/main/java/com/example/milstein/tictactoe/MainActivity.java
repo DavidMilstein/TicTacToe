ab56bf78c6cfd5649b087ccab008a28986075eab
David Milstein <david.milstein@hpe.com>
2018-11-04 18:54:35 +0200

    David - activity main intent implement

--- app/src/main/java/com/example/milstein/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/milstein/tictactoe/MainActivity.java
@@ -1,15 +1,41 @@
 package com.example.milstein.tictactoe;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-
+    private Button btnInstructions;
+    private Button btnStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnInstructions = findViewById(R.id.btnInsructionsID);
+        btnStart = findViewById(R.id.btnStartID);
+
+        btnInstructions.setOnClickListener(this);
+        btnStart.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if(v.getId() == R.id.btnInsructionsID) {
+            Intent in = new Intent(this, InstructionsActivity.class);
+            startActivity(in);
+        }
+        else {
+            Intent in = new Intent(this, GameActivity.class);
+            startActivity(in);
+        }
     }
 }
