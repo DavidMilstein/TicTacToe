5587924d53a179a862b79d6f7c5580896b12c877
David Milstein <david.milstein@hpe.com>
2018-11-06 14:07:32 +0200

    David - shared preference define

--- app/src/main/java/com/example/milstein/tictactoe/GameActivity.java
+++ app/src/main/java/com/example/milstein/tictactoe/GameActivity.java
@@ -1,5 +1,7 @@
 package com.example.milstein.tictactoe;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +10,11 @@ public class GameActivity extends AppCompatActivity {
 
     private GameBoard board;
     private char player = 'x';
+    private int currentXScore;
+    private int currentOScore;
+    private int currentDrawScore;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,9 +45,41 @@ public class GameActivity extends AppCompatActivity {
                 board.setPlayerAt(2,2, this.player);
         }
 
-        if(player=='x')
-            player='o';
-        else
-            player='x';
+        if(player == 'x') {
+            player = 'o';
+        }
+        else {
+            player = 'x';
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // read saved data from sheardPrefrences
+        // read the results of the games of all app lifecycle
+        SharedPreferences sp = getSharedPreferences("MyPref" , Context.MODE_PRIVATE);
+        currentXScore = sp.getInt("lastXScore",0);
+        currentOScore = sp.getInt("lastOScore",0);
+        currentDrawScore = sp.getInt("lastDrawScore",0);
+
+        //Log.d("debug", ">>>>>>>>>> onResume()");
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+
+        // save data (games result) into sheardPrefrences
+        SharedPreferences sp = getSharedPreferences("MyPref" , Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putInt("lastXScore",currentXScore);
+        editor.putInt("lastOScore",currentOScore);
+        editor.putInt("lastDrawScore",currentDrawScore);
+        editor.commit();
+
+       // Log.d("debug", ">>>>>>>>>> onPause()");
     }
 }
--- app/src/main/java/com/example/milstein/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/milstein/tictactoe/MainActivity.java
@@ -28,6 +28,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
+
+
     @Override
     public void onClick(View v) {
 
